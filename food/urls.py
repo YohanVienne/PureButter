bc0846cc9025aa4524d52d5a93557dc42e7f7d29
Yohan V <vienne.yohan@gmail.com>
2018-05-18 23:43:31 +0200

    fix urls

--- food/urls.py
+++ food/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('result/<str:search>',views.result, name='result'),
     path('product/', views.product, name='product'),
     path('product/<int:number>', views.product, name='product'),
