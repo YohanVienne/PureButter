152f72644aca4cb41f7675fd45ee79e09cb8d84a
Yohan V <vienne.yohan@gmail.com>
2018-05-18 23:39:26 +0200

    fix urls

--- pure_butter/urls.py
+++ pure_butter/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('food.urls')),
+    path('', include('food.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
