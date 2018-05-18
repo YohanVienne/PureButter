b219efe9188c63b897c07951d97309b4decc6d8c
Yohan V <vienne.yohan@gmail.com>
2018-05-19 00:53:05 +0200

    update heroku

--- pure_butter/settings.py
+++ pure_butter/settings.py
@@ -143,9 +143,6 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
-    )
 
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
