319a365158fa0a4a4f057733699d71ecb40eb48a
Yohan V <vienne.yohan@gmail.com>
2018-05-19 15:20:05 +0200

    ss

--- pure_butter/settings.py
+++ pure_butter/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 INTERNAL_IPS = ['127.0.0.1']
