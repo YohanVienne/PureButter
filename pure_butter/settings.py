e735ab88d8b9baeda9a447c08622060fef208495
Yohan V <vienne.yohan@gmail.com>
2018-05-19 19:24:59 +0200

    Production

--- pure_butter/settings.py
+++ pure_butter/settings.py
@@ -139,19 +139,9 @@ LOGIN_URL = '/connexion/'
 MEDIA_URL = '/media/'
 
 if os.environ.get('ENV') == 'PRODUCTION':
-
     # Static files settings
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
-    # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
-    )
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+  
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
