e627d190eab98485a8edf28ee9b042e9c7632843
Yohan V <vienne.yohan@gmail.com>
2018-05-19 15:03:38 +0200

    update

--- pure_butter/settings.py
+++ pure_butter/settings.py
@@ -139,10 +139,9 @@ MEDIA_URL = '/media/'
 
 if os.environ.get('ENV') == 'PRODUCTION':
     # Static files settings
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
